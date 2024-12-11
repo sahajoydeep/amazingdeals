ce520acd9fb63dc928fba2b11fc7749a878b5636
Joy <sahajoydeep2002@gmail.com>
2024-12-11 07:56:24 +0530

    commit

---
+++ src/Contact.js
@@ -0,0 +1,36 @@
+import "./mis.css"
+const Contact = () => (
+    <div className="contact">
+        <div className="joya">
+            <img src="joy.png" alt="Joy" className="joy"></img>
+            <div className="joyname"><p>Joydeep Saha</p></div></div>
+        <div className="front">
+            <p className="welcome">Hi There, Welcome to my website Amazing Deals India</p>
+            <p> My name is Joydeep Saha, I am currently a final year BSc CS Student at BITS-Pilani, I do web develping in my free
+                time as my hobby, finding my ways to get cheaper products and all, I met with an influencer than I knew, that you can many
+                items from Amazon, Flipkart, Meesho and other places for very cheap.</p>
+        </div>
+        <div className="description">
+            <p className="question">How this works?</p>
+
+            <p>Well, after talking with many people, one day I come across an influencer randomly at an event, where I found out that
+            they have cetain connetions, where sellers provide them cetain products for completely free for promotion, but you might think
+            what is it to me? right. I'm, not an influencer, trust me netiehr I am, but then, there are sellers who just listed their products
+            to amazon, flipkart and all,  where they need good ratings and all, for which, you order from amazon that product, and the seller reimburses
+            the ammount you paid, like you can get a 650 worth moisturerizer for completley free or just 59 INR.</p>
+
+            <p>You fill out some form, after ordering, then after receving the order, you submit review or rating, and then you submit the refund form.
+            All though, due to high number of forms and process, it takes about a month, but you do get it.</p>
+
+            So, I'm just sharing them with everyone, and my connections with differencet sellers and helping everyone.
+        </div>
+        <div className="email-section">
+            <p>You can Contact me Via Email:</p>
+            <a href="mailto:sahajoydeep4992@gmail.com" className="email-link">
+                <img src="email.png" alt="email" className="email-icon" /> </a>
+                <span className="email-text">sahajoydeep4992@gmail.com</span>
+                </div>
+    </div>
+);
+
+export default Contact;
